ebd650f0a8cbde08a9b4533d2e81217b38141db8
webquanlyphongtro <levandat100603@gmail.com>
2026-03-01 01:33:30 +0700

    Fix sai ten class CsrfToken cua Laravel 10

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
