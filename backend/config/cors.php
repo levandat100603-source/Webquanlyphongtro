289128d710ec7abb3a1088db869b9c680e34eb48
webquanlyphongtro <levandat100603@gmail.com>
2026-04-10 01:43:42 +0700

    a

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,12 +3,18 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => env('APP_ENV') === 'production' 
-        ? ['https://webquanlyphongtro-nhom3.vercel.app']
-        : ['*'],
+    
+    'allowed_origins' => [
+        'http://localhost:3000',                     
+        'http://localhost:5173',                      
+        'https://webquanlyphongtro-nhom3.vercel.app'  
+    ],
+    
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    
+    
+    'supports_credentials' => true,
 ];
