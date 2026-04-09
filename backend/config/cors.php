671b7ce67dd87bccc196237deee38d8def01908c
webquanlyphongtro <levandat100603@gmail.com>
2026-04-10 02:29:33 +0700

    aa

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -13,7 +13,7 @@ return [
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age' => 86400,
     
     
     'supports_credentials' => true,
